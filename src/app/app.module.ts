68726e8622700809b6ec4b673b27242e9fc02629
Robert Pirritano <rpirritano@gmail.com>
2019-02-11 18:23:12 -0800

    added bsic table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatInputModule,
+         MatCardModule,
+         MatButtonModule,
+         MatToolbarModule,
+         MatExpansionModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTableModule} from '@angular/material/table';
 import { AppComponent } from './app.component';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductComponent } from './components/product/product.component';
@@ -8,16 +16,27 @@ import { ProductComponent } from './components/product/product.component';
 import { ProductService } from './services/product.service';
 
 import { routing } from './app.routing';
+import { TableBasicExample } from './components/table/table-basic-example';
 
 @NgModule({
   declarations: [
     AppComponent,
     CartComponent,
-    ProductComponent
+    ProductComponent,
+    TableBasicExample
   ],
   imports: [
     BrowserModule,
-    routing
+    routing,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    HttpClientModule,
+    MatTableModule
   ],
   providers: [
     ProductService
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
-
+import { TableBasicExample } from './components/table/table-basic-example';
 import { CartComponent } from './components/cart/cart.component';
 import { ProductComponent } from './components/product/product.component';
 
 const routes: Routes = [
 	{ path: '', component: ProductComponent },
 	{ path: 'products', component: ProductComponent },
-	{ path: 'cart', component: CartComponent },
-	{ path: '**', redirectTo: '' }
+  { path: 'cart', component: CartComponent },
+  { path: 'table', component: TableBasicExample },
+  { path: '**', redirectTo: '' }
 ];
 
 export const routing = RouterModule.forRoot(routes);
